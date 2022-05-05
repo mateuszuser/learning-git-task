7d34fc4c353175c0688c1652e6681dc51f90b1d9
mateuszuser <ciomcia.mateusz@gmail.com>
2022-05-05 22:28:10 +0200

    add list quantity of products

--- task_git.py
+++ task_git.py
@@ -2,3 +2,6 @@ dict_of_products = {"piekarnia": ["chleb", "bułki", "paczki"], "warzywniak": ["
 y = 0
 for shop, products in dict_of_products.items():
     print(f"W sklepie {shop.capitalize()} i kupuję tam: {[i.capitalize() for i in products]}")
+    x: int = len(dict_of_products[shop])
+    y = y + x
+print(f"W sumie kupuję {y} produktów")
