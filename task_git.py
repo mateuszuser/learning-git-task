d21c5665a2578a6507d46e75bc579d4f44432a39
mateuszuser <ciomcia.mateusz@gmail.com>
2022-05-05 22:15:54 +0200

    add loop for

--- task_git.py
+++ task_git.py
@@ -1,1 +1,4 @@
 dict_of_products = {"piekarnia": ["chleb", "bułki", "paczki"], "warzywniak": ["marchew", "seler", "rukola"]}
+y = 0
+for shop, products in dict_of_products.items():
+    print(f"W sklepie {shop.capitalize()} i kupuję tam: {[i.capitalize() for i in products]}")
